f81b730078da8724e11b9cf5197c4d7887cb1a6e
Alex Bogdanov <abogdanov@gmail.com>
2021-04-10 22:52:34 -0700

    Light On, Light Off Server

--- main.py
+++ main.py
@@ -33,10 +33,24 @@ def dummy():
 
     return response_template % body
 
+pin = machine.Pin(16, machine.Pin.OUT)
+
+def light_on:
+     pin.value(1)
+     body = "You turned a light on!"
+     return response_template % body
+
+def light_off:
+     pin.value(0)
+     body = "You turned a light off!"
+     return response_template % body
 handlers = {
     'time': time,
     'dummy': dummy,
+    'light_on': light_on,
+    'light_off': light_off,
 }
+
 
 def main():
     s = socket.socket()
